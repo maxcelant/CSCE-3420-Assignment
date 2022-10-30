c9ac935ddbe894f75139f0d9918514747d661c4f
mcelant <massimiliano.celant@freeman.com>
2022-10-30 10:03:24 -0500

    finishing touches

---
+++ data.js
@@ -0,0 +1,92 @@
+const shoes = [
+    {
+        image: "shoes_pictures/shoe1.png",
+        brand: "Reebok",
+        type: "Arizona Slide Sandals - Women",
+        price: "$59.99",
+        rating: "&#9733;&#9733;&#9733;&#9733;&#9733;",
+        toggled: "false",
+        tag: "0001",
+        num: 3
+    },
+    {
+        image: "shoes_pictures/shoe4.png",
+        brand: "Birkenstock",
+        type: "Classic Harman Run Sneaker",
+        price: "$79.99",
+        rating: "&#9733;&#9733;&#9733;&#9733;&#9734;",
+        toggled: "false",
+        tag: "0002",
+        num: 6
+    },
+    {
+        image: "shoes_pictures/shoe7.png",
+        brand: "Marc Fisher",
+        type: "Isabel Bootie",
+        price: "$89.99",
+        rating: "&#9733;&#9733;&#9733;&#9733;&#9733;",
+        toggled: "false",
+        tag: "0003",
+        num: 2
+    },
+    {
+        image: "shoes_pictures/shoe2.png",
+        brand: "Converse",
+        type: "Chuck Taylor All Star",
+        price: "$39.99",
+        rating: "&#9733;&#9733;&#9733;&#9733;&#9733;",
+        toggled: "false",
+        tag: "0004",
+        num: 1
+    },
+    {
+        image: "shoes_pictures/shoe5.png",
+        brand: "adidas",
+        type: "Puremotion Sneaker",
+        price: "$69.99",
+        rating: "&#9733;&#9733;&#9733;&#9733;&#9733;",
+        toggled: "false",
+        tag: "0005",
+        num: 3
+    },
+    {
+        image: "shoes_pictures/shoe8.png",
+        brand: "Crocs",
+        type: "Classic Glitter Clog - Women's",
+        price: "$39.99",
+        rating: "&#9733;&#9733;&#9733;&#9734;&#9734;",
+        toggled: "false",
+        tag: "0006",
+        num: 6
+    },
+    {
+        image: "shoes_pictures/shoe6.png",
+        brand: "ASICS",
+        type: "Pulse 13 Sneaker",
+        price: "$99.99",
+        rating: "&#9733;&#9733;&#9733;&#9733;&#9733;",
+        toggled: "false",
+        tag: "0007",
+        num: 3
+    },
+    {
+        image: "shoes_pictures/shoe9.png",
+        brand: "New Balance",
+        type: "997H Sneaker",
+        price: "$49.99",
+        rating: "&#9733;&#9733;&#9733;&#9733;&#9734;",
+        toggled: "false",
+        tag: "0008",
+        num: 4
+    },
+    {
+        image: "shoes_pictures/shoe3.png",
+        brand: "Brooks",
+        type: "Ghost 14 Running - Women's",
+        price: "$89.99",
+        rating: "&#9733;&#9733;&#9733;&#9733;&#9734;",
+        toggled: "false",
+        tag: "0009",
+        num: 3
+    },
+];
